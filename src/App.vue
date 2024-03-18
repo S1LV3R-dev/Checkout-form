10c36de05cf69c295b99b7a501f26f6053b3a53f
S1LV3R-dev <seregaplotni@yandex.ru>
2024-03-18 06:52:11 +0000

    final changes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <script setup>
-    import cardInfo from './components/card_info.vue'
-    import orderInfo from './components/order_info.vue'
+import { ref } from 'vue';
+import cardInfo from './components/card_info.vue'
+import orderInfo from './components/order_info.vue'
+const price = ref(0);
+const orders = ref([
+    {
+        'id': 1,
+        'title': 'Title 1',
+        'description': 'Lorem, ipsum dolor',
+        'price': 150
+    },
+    {
+        'id': 2,
+        'title': 'Title 2',
+        'description': 'Lorem, ipsum dolor',
+        'price': 450
+    },
+    {
+        'id': 3,
+        'title': 'Title 3',
+        'description': 'Lorem, ipsum dolor',
+        'price': 500
+    }
+]);
+for (let i = 0; i < orders.value.length; i++) {
+    price.value += orders.value[i].price;
+}
 </script>
 
 <template>
     <div id="checkout_form">
-        <cardInfo></cardInfo>
-        <orderInfo></orderInfo>
+        <cardInfo :price="price"></cardInfo>
+        <orderInfo :orders="orders"></orderInfo>
     </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/card_info.vue
+++ src/components/card_info.vue
@@ -6,6 +6,8 @@ const mask = "####-####-####-####";
 const month = ref();
 const year = ref();
 const card_num = ref(mask);
+const props = defineProps(['price']);
+const price = props['price'];
 watch(month, (newValue, oldValue) => {
     console.log(newValue, oldValue)
     if (newValue != '') {
@@ -44,61 +46,65 @@ watch(year, (newValue, oldValue) => {
 </script>
 
 <template>
-    <form id="card_info">
-        <h3>Payment information</h3>
-        <div id="card">
-            <div id="chip_img">
-                <img src = '../assets/chip.svg' height="50px"/>
-            </div>
-            <span id="card_number"> {{ card_num }}</span>
-            <div id="card_bottom">
-                <div id="card_bottom_left">
-                    <div class="card_label">
-                        Card holder
-                    </div>
-                    <span>
-                        {{ name.toUpperCase() || "FULL NAME" }}
-                    </span>
+    <div id="card_info_block">
+        <form id="card_info">
+            <h3>Payment information</h3>
+            <div id="card">
+                <div id="chip_img">
+                    <img src='../assets/chip.svg' height="50px" />
                 </div>
-                <div id="card_bottom_right">
-                    <div class="card_label">
-                        Expires
+                <span id="card_number"> {{ card_num }}</span>
+                <div id="card_bottom">
+                    <div id="card_bottom_left">
+                        <div class="card_label">
+                            Card holder
+                        </div>
+                        <span>
+                            {{ name.toUpperCase() || "FULL NAME" }}
+                        </span>
                     </div>
-                    {{ month || "MM" }}/{{ year || "YY" }}
-                </div>
-            </div>
-        </div>
-        <div id="card_info_input">
-            <span class="card_info_label">Name on card</span>
-            <input type="text" class="card-info-input-field" v-model="name" />
-            
-            <span class="card_info_label">Card number</span>
-            <input type="text" class="card-info-input-field" @input="card_num = value + mask.slice(value.length)"
-                v-mask="mask" v-model="value" />
-            <div id="card_info_bottom">
-                <div id="card_info_expiration_date">
-                    <span class="card_info_label">Expiration date</span>
-                    <div id="card_info_inputs">
-                        <input class="card-info-input-field small-input-field" id="month" type="text" v-mask="'##'" @input="minmax" v-model="month" placeholder="Month" />
-                        <input class="card-info-input-field small-input-field" id="year" type="text" v-mask="'##'" v-model="year" placeholder="Year" />
-                    </div>
-                </div>
-                <div id="cvc">
-                    <span class="card_info_label">CVC</span>
-                    <div>
-                        <input class="card-info-input-field small-input-field" id="cvc_input" type="text" mask="'###'" />
+                    <div id="card_bottom_right">
+                        <div class="card_label">
+                            Expires
+                        </div>
+                        {{ month || "MM" }}/{{ year || "YY" }}
                     </div>
                 </div>
             </div>
-        </div>
-        <div id="price_tag">
-            {{ price }} USD
-        </div>
-        <div id="confirm">
-            <button>Confirm payment</button>
-        </div>
-    </form>
+            <div id="card_info_input">
+                <span class="card_info_label">Name on card</span>
+                <input type="text" class="card-info-input-field" v-model="name" />
+
+                <span class="card_info_label">Card number</span>
+                <input type="text" class="card-info-input-field" @input="card_num = value + mask.slice(value.length)"
+                    v-mask="mask" v-model="value" />
+                <div id="card_info_bottom">
+                    <div id="card_info_expiration_date">
+                        <span class="card_info_label">Expiration date</span>
+                        <div id="card_info_inputs">
+                            <input class="card-info-input-field small-input-field" id="month" type="text" v-mask="'##'"
+                                @input="minmax" v-model="month" placeholder="Month" />
+                            <input class="card-info-input-field small-input-field" id="year" type="text" v-mask="'##'"
+                                v-model="year" placeholder="Year" />
+                        </div>
+                    </div>
+                    <div id="cvc">
+                        <span class="card_info_label">CVC</span>
+                        <div>
+                            <input class="card-info-input-field small-input-field" id="cvc_input" type="text"
+                                mask="'###'" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="price_tag">
+                {{ price }} USD
+            </div>
+            <div id="confirm">
+                <button>Confirm payment</button>
+            </div>
+        </form>
+    </div>
 </template>
 
-<style>
-</style>
+<style></style>
